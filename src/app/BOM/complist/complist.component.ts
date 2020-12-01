4c9e8bd84ee73a0dc91374234fb6e43319723002
DrPokst <62307971+DrPokst@users.noreply.github.com>
2020-12-01 15:42:34 +0200

    job list

--- src/app/BOM/complist/complist.component.ts
+++ src/app/BOM/complist/complist.component.ts
@@ -6,6 +6,7 @@ import { BomList } from 'src/app/_models/BomList';
 import { xBomList } from 'src/app/_models/xBomList';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { BOMService } from 'src/app/_services/BOM.service';
+import { ReelService } from 'src/app/_services/reel.service';
 
 @Component({
   selector: 'app-complist',
@@ -19,7 +20,8 @@ export class ComplistComponent implements OnInit {
   model: any = {};
   check = false;
 
-  constructor(private router: Router, private bomService: BOMService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+  constructor(private router: Router, private bomService: BOMService,
+              private alertify: AlertifyService, private route: ActivatedRoute, private reelService: ReelService) { }
 
   ngOnInit(): void {
     this.GetBomList();
@@ -48,6 +50,14 @@ export class ComplistComponent implements OnInit {
       this.alertify.error(error);
     });
     this.check = true;
+  }
+
+  TurnOnLed(id: number){
+    this.reelService.TurnOnLed(id).subscribe(()=>{
+      this.alertify.success('sekmingai uzdegta');
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 
